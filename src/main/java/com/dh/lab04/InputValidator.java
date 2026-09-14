b6bef1e7e61f2e3b52845ef3ed3309ff9c0bde79
danielh281 <daniel.haddadeen1@gmail.com>
2026-09-14 11:02:33 -0400

    Updated InputValidator Class

--- src/main/java/com/dh/lab04/InputValidator.java
+++ src/main/java/com/dh/lab04/InputValidator.java
@@ -1,6 +1,8 @@
 package com.dh.lab04;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import javafx.scene.control.TextField;
@@ -10,7 +12,11 @@ import javafx.scene.control.TextField;
  * @author Daniel Haddadeen
  */
 public class InputValidator {
-    public static InformationField[] checkFieldsValid() {
+    /**
+     * Checks whether every information field was properly inputted and formatted.
+     * @return The list of invalid information fields
+     */
+    public static List<InformationField> checkFieldsValid() {
         UserInformation userInformation = App.getUserInformation();
         Map<InformationField, TextField> inputFields = userInformation.getInputFields();
         
@@ -18,19 +24,62 @@ public class InputValidator {
         
         for (InformationField informationField : InformationField.values()) {
             TextField inputField = inputFields.get(informationField);
-            
             String input = inputField.getText();
             
             // Removes spaces at start and end
             input.trim();
             
+            // If the field is empty but its optional then skip to the next
+            if (informationField.isOptional && input.isEmpty()) {
+                continue;
+            }
+            
+            // Attempt to convert input to integer IF the input is for days
+            if (informationField.equals(InformationField.DAYS)) {
+                try {
+                    Integer value = Integer.valueOf(input);
+                } catch(NumberFormatException e) {
+                    invalidFields.add(informationField);
+                }
+                
+                continue;
+            }
+            
+            // Attempt to convert input to double otherwise
             try {
                 Double value = Double.valueOf(input);
             } catch(NumberFormatException e) {
-                System.out.println("Invalid field" + informationField);
+                invalidFields.add(informationField);
             }
         }
         
-        return null;
+        return invalidFields;
+    }
+    
+    /**
+     * Gets the user input from all the input fields. Defaults to 0 for optional empty fields.
+     * @return The map of information field to the user input
+     */
+    public static Map<InformationField, Number> getUserInput() {
+        UserInformation userInformation = App.getUserInformation();
+        Map<InformationField, TextField> inputFields = userInformation.getInputFields();
+        
+        Map<InformationField, Number> userInput = new HashMap<>();
+        
+        for (InformationField informationField : InformationField.values()) {
+            TextField inputField = inputFields.get(informationField);
+            String input = inputField.getText();
+            
+            // Removes spaces at start and end
+            input.trim();
+
+            // Default to 0.0 for optional fields with no input
+            if (informationField.isOptional && input.isEmpty()) {
+                userInput.put(informationField, 0.0);
+                continue;
+            }
+            
+            userInput.put(informationField, Double.valueOf(input));
+        }
     }
 }
