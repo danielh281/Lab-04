149cefbec634bc55af3874c46839960aa9f80e1a
danielh281 <daniel.haddadeen1@gmail.com>
2026-09-14 19:01:58 -0400

    Added GitHub link to App Class

--- src/main/java/com/dh/lab04/App.java
+++ src/main/java/com/dh/lab04/App.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+// GitHub Repository Link: https://github.com/danielh281/Lab-04/
 
 /**
  * JavaFX App
